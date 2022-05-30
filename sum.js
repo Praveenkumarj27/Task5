3e10f413403e44762c053d3975eee397050b18cf
Praveenkumar.J <103561511+Praveenkumarj27@users.noreply.github.com>
2022-05-30 08:42:01 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -23,3 +23,18 @@ var sum = function(b){
     return res
 }
 console.log(sum(b))
+
+
+// Using Sum in Arrow function
+
+let c=[1,2,3,4,5,6,7];
+var sum1= (c)=>{
+    var  res=0;
+    for(i=0;i<c.length;i++)
+    {
+            res+=c[i];
+           
+    }
+    return res
+}
+console.log(sum1(c))
