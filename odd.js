e14856ad58537f0268163edbee5340612050eede
Praveenkumar.J <103561511+Praveenkumarj27@users.noreply.github.com>
2022-05-30 08:39:04 +0530

    Update odd.js

--- odd.js
+++ odd.js
@@ -37,3 +37,22 @@ console.log(odd(b));
 
 
 
+
+
+
+// Using odd numbers in anonymous
+
+var c=[1,2,3,4,5,11];
+
+var odd2=(a)=>{
+ var result="";
+ for (const key of c) {
+    if (key%2!==0) {
+        result+=key+" "
+    } 
+ }
+    return result;
+}
+console.log(odd2(c));
+
+
